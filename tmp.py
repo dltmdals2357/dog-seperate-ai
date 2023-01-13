c54c203293edaea795876597393371bb38542a25
dltmdals2357 <102795179+dltmdals2357@users.noreply.github.com>
2023-01-13 20:15:37 +0900

    Update and rename tmp.py to use_bingimagedownloader.py

--- tmp.py
+++
@@ -1,3 +0,0 @@
-from bing_image_downloader import downloader
-downloader.download("강아지 진돗개 사진", limit=50, output_dir="korean_jindo", adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
-downloader.download("강아지 시추 사진", limit=50, output_dir="Shih Tzu", adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
---
+++ use_bingimagedownloader.py
@@ -0,0 +1,3 @@
+from bing_image_downloader import downloader
+downloader.download("korean jindo image", limit=50, output_dir="korean_jindo", adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
+downloader.download("ShiTzu image", limit=50, output_dir="Shih Tzu", adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
